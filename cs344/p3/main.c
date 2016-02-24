4f6c46d65b2f0b490a3d969482a912b06b1b57dd
Isaac Stallcup <stallcui@oregonstate.edu>
2016-02-23 18:06:43 -0800

    began to add cd functionality

--- cs344/p3/main.c
+++ cs344/p3/main.c
@@ -9,34 +9,41 @@ int main()
 	printf("Welcome to smallsh, the smaller shell.\n");
 	printf("\n");
 
-	while (1)
-	{
-	
-	printf(": ");
-	fflush(stdout);
-
-	char* argv[1];
-	scanf("%s",*argv);
-
-	printf(": ");
-	fflush(stdout);
-
 	pid_t childPid;
 
-	switch(childPid = fork())
+	while (1)
 	{
-		case -1:
-			perror("Error in forking to child after input.\n");
-			exit(-1);
-			break;
-		case 0:
-			if (-1 == execvp(*argv,argv))
-				printf("error\n");
-			break;
-		default:
-			waitpid(childPid);
-			break;
-	}
+		printf(": ");
+		fflush(stdout);
+	
+		/* changes directory to "demo" */
+		chdir("./demo");
+
+		char* argv[1];
+		scanf("%s",*argv);
+		
+		if (strcmp(*argv,"exit") == 0)
+			exit(0);
+		
+
+		printf(": ");
+		fflush(stdout);
+	
+	
+		switch(childPid = fork())
+		{
+			case -1:
+				perror("Error in forking to child after input.\n");
+				exit(-1);
+				break;
+			case 0:
+				if (-1 == execvp(*argv,argv))
+					printf("error\n");
+				break;
+			default:
+				waitpid(childPid);
+				break;
+		}
 	}
 	return 0;
 }
