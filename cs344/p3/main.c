8ae5a580b5d4d767d5bfba0eadc256d470252073
Isaac Stallcup <stallcui@oregonstate.edu>
2016-02-29 19:43:32 -0800

    got redirection

--- cs344/p3/main.c
+++ cs344/p3/main.c
@@ -8,7 +8,6 @@
 
 char** tokcmd(char** cmdline,size_t max_size)
 {
-	int argNum;
 	int argc = 0;
 	char** argTok = malloc(sizeof(char*)*max_size);
 	argTok[argc] = strtok(*cmdline," \n"); //name of command
@@ -18,57 +17,103 @@ char** tokcmd(char** cmdline,size_t max_size)
 		argTok[argc] = strtok(NULL," \n"); //arguments
 	}
 
-	//testing area
-	/*printf("argc=%d\n",argc);
-	int i;
-	for (i = 0; i < argc; i++)
-	{
-		printf("argTok[%d]=%s\n",i,argTok[i]);
-	}*/
-
-
 	free(argTok);
 	return argTok;
 }
 
-char** getcmd(char** argv)
+char** getcmd(char** argv, size_t max_size)
 {
-	size_t max_size = 512;
+	fflush(stdin);
 	getline(argv,&max_size,stdin);
 	argv = tokcmd(argv,max_size);
-	int i;
-	/*for (i = 0; i < max_size; i++)
-	{
-		if (argv[i])
-			printf("argv[%d]=%s\n",i,argv[i]);
-		else break;
-	}
-	fflush(stdout);*/
 	return argv;
 }
 
-int isRedirect(char* arg)
+int isbkgrnd(char** argv, char* amp, size_t max_size)
 {
-	if (strcmp(arg,"|") == 0)
-		return 1;
-	else if (strcmp(arg,"<") == 0)
-		return 2;
-	else if (strcmp(arg,">") == 0)
-		return 3;
-	else return 0;
+	int argNum;
+	for(argNum = 0; argNum < max_size; argNum++)
+	{
+		if (argv[argNum] == NULL)
+			break;
+		if (strcmp(argv[argNum],amp) == 0)
+		{
+			//printf("%s = &\n",argv[argNum]);
+			return 1;
+		}
+	}
+	return 0;	
 }
 
-int fdchg(char** argv, size_t max_size)
+int redirectType(char* arg)
 {
-	int argNum,redType;
-	printf("start fdchg\n");
+	
+	if (strcmp(arg,"<") == 0) //READING
+		return 0;
+	else if (strcmp(arg,">") == 0) //WRITING
+		return 1;
+	else return -1; //no redirection
+}
+
+int chgfd(char** argv, size_t max_size)
+{
+	int argNum, fd_old, fd_new;
+	char* arg;
+	char* path;
 	for (argNum = 0; argNum < max_size; ++argNum)
 	{
-		redType = isRedirect(argv[argNum]);
-		printf("Look@%s\n",argv[argNum]);
-		if (redType == 0)
-			printf("not a redirection character:%s\n",argv[argNum]);
-		else printf("%s is a redirection character\n",argv[argNum]);
+		arg = argv[argNum];
+		if(!arg)
+			break;
+		fd_old = redirectType(arg);
+		//printf("rType = %d\n",fd_old);
+		if(fd_old == 0)
+		{
+			path = argv[argNum-1];
+			printf("path=%s\n",path);
+			printf("< detected\n");
+			printf("stdin is now from file %s\n",argv[argNum-1]);
+			fd_new = open(path,O_RDONLY);
+			if(fd_new == -1)
+			{
+				fprintf(stderr, "Error in file open\n");
+				close(fd_new);
+				exit(1);
+			}
+			else if (dup2(fd_new,fd_old) == -1)
+			{
+				fprintf(stderr,"Error in dup2\n");
+				close(fd_new);
+				exit(1);
+			}
+			close(fd_new);
+			argv[argNum] = NULL;
+			argv[argNum-1] = NULL;
+
+		}
+		else if (fd_old == 1)
+		{
+			path = argv[argNum+1];
+			printf("path=%s\n",path);
+			printf("> detected\n");
+			printf("stdout is now to file %s\n",argv[argNum+1]);
+			fd_new = open(path,O_WRONLY);
+			if(fd_new == -1)
+			{
+				fprintf(stderr, "Error in file open\n");
+				close(fd_new);
+				exit(1);
+			}
+			else if (dup2(fd_new,fd_old) == -1)
+			{
+				fprintf(stderr,"Error in dup2\n");
+				close(fd_new);
+				exit(1);
+			}
+			close(fd_new);
+			argv[argNum] = NULL;
+			argv[argNum+1] = NULL;
+		}
 	}
 	return 0;
 }
@@ -84,24 +129,28 @@ int main()
 	char** argv = malloc(sizeof(char*)*max_size);
 
 	int status;
+	int background;
 
 	while(1)
 	{
-			
+		background = 0;	
+
 		fflush(stdout);
 		printf(": ");
 	
-		argv = getcmd(argv);
-
-
-		//replace this with a function? we'll see
+		argv = getcmd(argv, max_size);
+		//find & at end of user's input
+		background = isbkgrnd(argv,"&", max_size);
+		
 
 		//builtin: exit
 		//exits program by breaking loop
+		//needs work, as does not work with implementation of cd
 		if (strcmp(*argv,"exit") == 0)
-				break;
+			break;
 		//builtin: cd
 		//changes directories
+		//needs to appropriately kill child/parent processes
 		else if (strcmp(*argv,"cd") == 0)
 		{
 			switch(childPid = fork())
@@ -137,22 +186,23 @@ int main()
 		switch(childPid = fork())
 		{
 			case -1:
-				perror("smallsh: fork failed\n");
+				fprintf(stderr,"smallsh: fork failed\n");
 				exit(1);
 				break;
 			case 0:
+				//need to edit args for background, redirections
+				chgfd(argv,max_size);	
 				if (-1 == execvp(*argv,argv))
 				{
-					perror("smallsh");
+					perror(*argv);
 					exit(2);
 				}
-				fflush(stdout);
 				break;
 			default:
+				printf("parent\n");
 				waitpid(childPid,&status,0);
 				break;
 		}
 	}
-	free(*argv);
 	return 0;
 }
